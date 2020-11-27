8ee619958e713c5e52a41fd60a0dc093263c3c1a
Nurzhan Akhmetov <nurakmedy@gmail.com>
2020-11-27 11:09:01 +0600

    Изменил namespace для ExceptionMiddleware.cs

--- THA.Search.App/ExceptionMiddleware.cs
+++ THA.Search.App/ExceptionMiddleware.cs
@@ -3,7 +3,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 
-namespace THA.Search.Mocks
+namespace THA.Search.App
 {
     public sealed class ExceptionMiddleware
     {
